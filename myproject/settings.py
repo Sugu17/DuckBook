1b7b7e98258d555be8bce96fa47245e686e0a383
WadeWilson <sugumarsg17@gmail.com>
2023-04-22 19:14:29 +0530

    Added whitenoise static backend

--- myproject/settings.py
+++ myproject/settings.py
@@ -154,11 +154,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-if not DEBUG:   
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 #STATICFILES_DIRS = ("static",)
 
