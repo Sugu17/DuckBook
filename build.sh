3e5fdf7e8b7356fe945051a53ebea421337cf841
WadeWilson <sugumarsg17@gmail.com>
2023-04-23 01:19:27 +0530

    Added S3 static storage

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-#python manage.py migrate
+python manage.py migrate
 if [ $CREATE_SUPERUSER == true ]
 then
   python manage.py createsuperuser --no-input
