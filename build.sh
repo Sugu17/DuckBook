c35fb3241602e00ed20e59f040f976b29afd0ce2
WadeWilson <sugumarsg17@gmail.com>
2023-04-22 20:34:59 +0530

    Added whitenoise static backend

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 #python manage.py collectstatic --no-input
 python manage.py migrate
-
+python manage.py runworker --settings=myproject.settings -v2
 if [ $CREATE_SUPERUSER ];
 then
   python manage.py createsuperuser --no-input
