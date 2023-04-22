7bda2a9a62fef474ff0256dfb2dc1b54239b5b8b
WadeWilson <sugumarsg17@gmail.com>
2023-04-22 18:15:46 +0530

    Refactor settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -152,7 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if not DEBUG:   
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = ("static",)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
